21eec4831c70b8460a261c17ac4a28e85eaf0071
Ya-An Hsiung <ya-anhsiung@berkeley.edu>
2017-03-10 15:45:46 -0800

    idk

---
+++ PokedexLab/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  PokedexLab
+//
+//  Created by YHsiung on 3/2/17.
+//  Copyright © 2017 iOS Decal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageViewOutlet: UIImageView!
+    
+}
--- PokedexLab/SearchViewController.swift
+++ PokedexLab/SearchViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
     var pokemonArray: [Pokemon] = []
     var filteredArray: [Pokemon] = []
@@ -16,6 +16,8 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         pokemonArray = PokemonGenerator.getPokemonArray()
+        collectionViewOutlet.delegate = self
+        collectionViewOutlet.dataSource = self
 
     }
 
@@ -23,6 +25,33 @@ class SearchViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
+    @IBOutlet weak var collectionViewOutlet: UICollectionView!
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return PokemonGenerator.categoryDict.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionViewOutlet.dequeueReusableCell(withReuseIdentifier: "prototype", for: indexPath) as! CollectionViewCell
+        cell.imageViewOutlet.image = UIImage(named: PokemonGenerator.categoryDict[indexPath.row]!)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.filteredArray = filteredPokemon(ofType: indexPath.row)
+        performSegue(withIdentifier: "searchToCategory", sender: Any?.self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            if identifier == "searchToCategory" {
+                if let dest = segue.destination as? CategoryViewController {
+                    dest.pokemonArray = self.filteredArray
+                }
+            }
+        }
+    }
     
     // Utility function to iterate through pokemon array for a single category
     func filteredPokemon(ofType type: Int) -> [Pokemon] {
