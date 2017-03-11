ee8f27b4dc1a7dffeb45951616d7531853831019
Ya-An Hsiung <ya-anhsiung@berkeley.edu>
2017-03-10 17:24:45 -0800

    done i think

--- PokedexLab/CategoryViewController.swift
+++ PokedexLab/CategoryViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class CategoryViewController: UIViewController {
+class CategoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var PokemonTableView: UITableView!
     
     var pokemonArray: [Pokemon]?
     var cachedImages: [Int:UIImage] = [:]
@@ -16,11 +18,69 @@ class CategoryViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        PokemonTableView.delegate = self
+        PokemonTableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (pokemonArray?.count)!
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = PokemonTableView.dequeueReusableCell(withIdentifier: "pokemonRow", for: indexPath) as! PokemonTableViewCell
+        let pokemon: Pokemon! = pokemonArray![indexPath.row]
+        if let image = cachedImages[indexPath.row] {
+            cell.pokemonImageView.image = image // may need to change this!
+        } else {
+            let url = URL(string: pokemon.imageUrl)!
+            let session = URLSession(configuration: .default)
+            let downloadPicTask = session.dataTask(with: url) { (data, response, error) in
+                if let e = error {
+                    print("Error downloading picture: \(e)")
+                } else {
+                    if let _ = response as? HTTPURLResponse {
+                        if let imageData = data {
+                            let image = UIImage(data: imageData)
+                            self.cachedImages[indexPath.row] = image
+                            cell.pokemonImageView.image = UIImage(data: imageData) // may need to change this!
+                            
+                        } else {
+                            print("Couldn't get image: Image is nil")
+                        }
+                    } else {
+                        print("Couldn't get response code")
+                    }
+                }
+            }
+            downloadPicTask.resume()
+        }
+        cell.pokemonName.text = pokemon?.name
+        cell.pokemonNumber.text = "#\(String(pokemon.number))"
+        cell.pokemonStats.text = "\(pokemon.attack!)/\(pokemon.defense!)/\(pokemon.health!)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedIndexPath = indexPath
+        performSegue(withIdentifier: "categoryToPokeInfo", sender: Any?.self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            if identifier == "categoryToPokeInfo" {
+                if let dest = segue.destination as? PokemonInfoViewController {
+                    dest.pokemon = pokemonArray?[(selectedIndexPath?.row)!]
+                    dest.image = cachedImages[(selectedIndexPath?.row)!]
+                }
+            }
+        }
+    }
+
 
 
 }
---
+++ PokedexLab/PokemonTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonTableViewCell.swift
+//  PokedexLab
+//
+//  Created by YHsiung on 3/10/17.
+//  Copyright © 2017 iOS Decal. All rights reserved.
+//
+
+import UIKit
+
+class PokemonTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var pokemonImageView: UIImageView!
+    @IBOutlet weak var pokemonName: UILabel!
+    @IBOutlet weak var pokemonNumber: UILabel!
+    @IBOutlet weak var pokemonStats: UILabel!
+    
+}
